616ed2b672b567e4c3e262a31914d210bf018df7
gramirezgh <guerynextu@gmail.com>
2017-09-08 00:37:40 -0400

    actividad de codigo Uno EFectos
    actividad de codigo Uno EFectos

---
+++ EFECTOS JQUERY/COLA DE EFECTOS/ACTIVIDAD DE CODIGO UNO/actividadDeCodigoUno.js
@@ -0,0 +1,20 @@
+$(function () {
+    $("#boton1").click(function () {
+        colaEfectos();
+    });
+    function colaEfectos() {
+        var miDiv = $("#midiv1");
+        miDiv.queue(function () {
+            $(this).css({
+                "background-color":"blue",
+            });
+            $("#mensajeFinal").html("Se cambio el color a fondo azul");
+            $(this).dequeue();
+        });
+        miDiv.hide(1000);
+        miDiv.show(1000);
+        miDiv.fadeIn(3000);
+        miDiv.fadeOut(8000);
+        miDiv.show(1000);
+    }
+});
