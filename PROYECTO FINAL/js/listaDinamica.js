aba95eff19234fc347b92d70f2bc62a44cdef4b4
gramirezgh <guerynextu@gmail.com>
2017-09-22 00:42:46 -0400

    cambio 1 Final PRyecto Final JQUERY
    cambio 1 Final PRyecto Final JQUERY

--- PROYECTO FINAL/js/listaDinamica.js
+++ PROYECTO FINAL/js/listaDinamica.js
@@ -1,164 +1,5 @@
-// //JSON de estudiantes
-// var estudiantes = [
-//     {codigo:"020",nombre:"Andres Copa",nota:4,asistencia:6,genero:"M"},
-//     {codigo:"019",nombre:"Arturo Mendez",nota:1,asistencia:6,genero:"M"},
-//     {codigo:"018",nombre:"Liliana Carvajal",nota:9,asistencia:8,genero:"F"},
-//     {codigo:"001",nombre:"Jhoel Martinez",nota:10,asistencia:9,genero:"M"}
-// ];
-// //cargar los eventos a traves de WINDOWS ONLOAD
-// window.onload = cargarEventos;
-// //Funcion que realiza la carga de eventos  a traves de addEventListener
-// function cargarEventos(){
-//     document.getElementById("mostrar-tabla").addEventListener("click",mostrarTabla,false);
-//     document.getElementById("nuevo-estudiante").addEventListener("submit",nuevoEstudiante,false);
-//     document.getElementById("nota-promedio").addEventListener("click",notaPromedio,false);
-//     document.getElementById("nota-mayor-menor").addEventListener("click",notaMayorMenor,false);
-//     document.getElementById("promedio-asistencia").addEventListener("click",promedioAsistencia,false);
-//     document.getElementById("mostrar-genero").addEventListener("click",mostrarGenero,false);
-// }
-// //funcion que muestra la tabla Estudiantes mediante una iteracion y posterior extraccion de JSON estudiantes
-// function mostrarTabla(){
-//     var cuerpoTabla = document.getElementById("estudiantes-tabla");
-//     var tablaLlena="";
-//
-//     for(var i=0;i<estudiantes.length;i++){
-//         tablaLlena += "<td><td>"+estudiantes[i].codigo+"</td><td>"+estudiantes[i].nombre+
-//                       "</td><td>"+estudiantes[i].nota+"</td><td>"+estudiantes[i].asistencia+
-//                       "</td><td>"+estudiantes[i].genero+"</td></tr>";
-//     }
-//     cuerpoTabla.innerHTML=tablaLlena;
-// }
-// //Funcion que añade un estudiante nuevo a la tabla Estudiantes
-// function nuevoEstudiante(event) {
-//     event.preventDefault();
-//
-//     var obtenerValueCodigo = document.getElementById("codigo").value;
-//     var obtenerValueNombre = document.getElementById("nombre").value;
-//     var obtenerValueNota = document.getElementById("nota").value;
-//     var obtenerValueAsistencia = document.getElementById("asistencia").value;
-//     var obtenerValueGenero = document.getElementById("genero").value;
-//
-//     var nuevoEstudiante = {codigo:obtenerValueCodigo, nombre:obtenerValueNombre,
-//                             nota:obtenerValueNota, asistencia:obtenerValueAsistencia,
-//                             genero:obtenerValueGenero};
-//     estudiantes.push(nuevoEstudiante);
-// }
-// //Funcion que permite calcular el promedio de notas
-// function promedio(json){
-//     var p = 0;
-//     var q = 0;
-//     var r = 0;
-//     var i;
-//     for (var i = 0; i < json.length; i++) {
-//         p = json[i].nota;
-//         j = i+1;
-//         q = (q+p);
-//         r = Math.round(q/j);
-//     }
-//     alert("El promedio de notas es : "+r);
-// }
-// //Funcion que carga a la funcion promedio
-// function notaPromedio(){
-//     promedio(estudiantes);
-// }
-// //Funcion que halla la nota mayor y menor de la Tabla estudiantes
-// function mayorMenor(json){
-//     var min = null;
-//     var max = null;
-//     for (var i = 0, len = estudiantes.length; i < len; i++) {
-//         var elem = estudiantes[i].nota;
-//         if (min === null || min > elem) min = elem;
-//         if (max === null || max < elem) max = elem;
-//     }
-//     alert( "La menor nota es = " + min + ", La mayor nota es = " + max );
-// }
-// //Funcion que carga la funcion mayorMenor
-// function notaMayorMenor(){
-//     mayorMenor(estudiantes);
-// }
-// //Funcion que calcula el promedio de asistencia en la Tabla Estudiantes
-// function obtenerPromedioAsistencia(json){
-//     var p = 0;
-//     var q = 0;
-//     var s = 0;
-//     var i;
-//     for (var i = 0; i < json.length; i++) {
-//         p = json[i].asistencia;
-//         j = i+1;
-//         q = (q+p);
-//         s = Math.round(q/j);
-//     }
-//     alert("El promedio de asistencia es : "+s);
-// }
-// //Funcion que carga a la funcion obtenerPromedioAsistencia
-// function promedioAsistencia(){
-//     obtenerPromedioAsistencia(estudiantes);
-// }
-// //Funcion que permite calcular la cantiudad de Hombres y Mujeres en la Tabla Estudiantes
-// function obtenerGenero(json){
-//     var f = 0;
-//     var m = 0;
-//     for (var i = 0; i < json.length; i++) {
-//         if (json[i].genero=="F" || json[i].genero=="f") {
-//             f = f+1;
-//         }
-//         else{
-//             m = m+1;
-//         }
-//     }
-//     alert("La cantidad de mujeres es : "+f+" y la cantidad de hombres es : "+m);
-// }
-// //Funcion que carga la funcion obtenerGenero
-// function mostrarGenero(){
-//     obtenerGenero(estudiantes);
-// }
-//
-// /////INTENTOS EN LA FUNCION MAYOR MENOR
-// // function mayorMenor(json){
-// //     // var i = 0;
-// //     // var k = [];
-// //     var min = null;
-// //     var max = null;
-// //     for (var i = 0, len = estudiantes.length; i < len; i++) {
-// //         // if((json[i].nota > json[j].nota)&&(i==j))
-// //         // {
-// //         //     // alert("La nota mayor es : "+json[i].nota+" y el nombre del alumno es : "+json[i].nombre);
-// //         //     //  document.getElementById("c").innerHTML=("Nombre : "+json[i].nombre+"<br> Nota :"+json[i].nota);
-// //         //     // var listaDeNotas = [estudiantes[i].nota];
-// //         //     // i++;
-// //         // }
-// //         // var maxArray = Function.prototype.apply.bind(Math.max, null);
-// //         // var maxArray = estudiantes[j].nota;
-// //         // var b = estudiantes[j].nota;
-// //         // var c = estudiantes[j].nota;
-// //         // var d = estudiantes[j].nota;
-// //         //  console.log(maxArray);
-// //         // listaNotas = k.push(estudiantes[j].nota);
-// //
-// //         // var listaUno = document.getElementById("lista-de-notas").innerHTML = estudiantes[j].nota;
-// //         //  var maximo = Math.max(estudiantes[0].nota,estudiantes[1].nota,estudiantes[2].nota,estudiantes[3].nota,estudiantes[4].nota);
-// //         // var maximo = Math.max(estudiantes.nota);
-// //         // alert(maximo);
-// //         // listaUno.innerHTML = estudiantes[j].nota;
-// //
-// //
-// //         // var listaNumeros = document.write("Posición " + j + " del array: " + estudiantes[j].nota)
-// //         // document.write("<br>")
-// //         // document.getElementById("lista-de-notas").innerHTML = listaNumeros;
-// //         // alert(listaNumeros);
-// //
-// //         // return Math.min.apply(Math,json.nota);
-// //         var elem = estudiantes[i].nota;
-// //         if (min === null || min > elem) min = elem;
-// //         if (max === null || max < elem) max = elem;
-// //     }
-// //     alert( "min = " + min + ", max = " + max );
-// //     // document.getElementById("lista-de-notas").innerHTML = maxArray;
-// // }
-// $(function () {
 
-// var jsonData={};
-// jsonData.characters = [];
+////Funcion para Editar Estudiantes
     function editarEstudiante(id) {
         var estudiante;
         for(var i=0; i<localStorage.length; i++)
@@ -175,11 +16,12 @@
             }
         }
     }
-
+//////Funcion para Listar Estudiantes
     function listarEstudiante() {
         var tabla="";
         var parrafo1=$("#p1");
-
+        tabla += '<center>';
+        tabla += '<h1>TABLA DE ESTUDIANTES</h1><br><br>';
         tabla += '<table border="1">';
         tabla += '<tr>';
         tabla += '<th>CODIGO</th>';
@@ -188,12 +30,13 @@
         tabla += '<th>ASISTENCIA</th>';
         tabla += '<th>GENERO</th>';
         tabla += '</tr>';
-
+        tabla += '</center>';
         for(var i=0; i<localStorage.length;i++)
         {
+         ///Obteniendo valores de localStorage
             var clave = localStorage.key(i);
             var estudiante = $.parseJSON(localStorage.getItem(clave));
-
+         ///Añadiendo valores a la Tabla Estudiante
             tabla += '<tr>';
             tabla += '<td>'+estudiante.codigo+'</td>';
             tabla += '<td>'+estudiante.nombre+'</td>';
@@ -203,38 +46,84 @@
             tabla += '<td><button onclick="editarEstudiante(\''+estudiante.codigo+'\');">Editar</button></td>';
             tabla += '<td><button onclick="eliminarEstudiante(\''+estudiante.codigo+'\');">Eliminar</button></td>';
             tabla += '</tr>';
-
-            // localStorage.setItem("jsonData", estudiante);
-            // localStorage.setItem("jsonData", JSON.stringify(estudiante));
-            // var jsonDataa = JSON.parse(localStorage.getItem("jsonData"));
-            // console.log(jsonDataa);
-
-
-
-        }
+         }
         tabla += '</table>';
         $(parrafo1).html(tabla);
 
-        //Invocando al boton mostrar genero despues de haber sido poblado
-        $("#mostrar-genero").click(function () {
+        ///Funcion para Obtener la Nota Promedio de la Lista de Estudiantes
+        $("#nota-promedio").click(function () {
+                var uno = 0;
+                var dos = 0;
+                var total = 0;
             for(var i=0; i<localStorage.length;i++){
-                var q=0;
+                ///Obteniendo valores de LocalStorage
                 var clave = localStorage.key(i);
                 var estudiante = $.parseJSON(localStorage.getItem(clave));
-                var estudianteNota = estudiante.nota;
-                j = i+1;
-                q = (q+estudianteNota);
-                r = Math.round(q/j);
-
-
+                var estudianteNota = parseInt(estudiante.nota);
+                j=i+1;
+                uno = estudianteNota;
+                dos = parseInt(uno+dos);
+                total = Math.round(dos/j);
             }
-
-            console.log(r);
+            alert("La nota promedio es : "+total);
+          });
+        ///Funcion para obtener la Mayor y Menor Nota de la Lista de Estudiantes
+        $("#nota-mayor-menor").click(function () {
+            var min = null;
+            var max = null;
+            for (var i = 0, len = localStorage.length; i < len; i++) {
+                var clave = localStorage.key(i);
+                var estudiante = $.parseJSON(localStorage.getItem(clave));
+                var elem = estudiante.nota;
+                if (min === null || min > elem) min = elem;
+                if (max === null || max < elem) max = elem;
+            }
+            alert( "La menor nota es = " + min + ", La mayor nota es = " + max );
         });
 
+        ///Funcion para obtener el Promedio de Asistencia de la Lista de Estudiantes
+        $("#promedio-asistencia").click(function () {
+            var uno = 0;
+            var dos = 0;
+            var total = 0;
+            for(var i=0; i<localStorage.length;i++){
+                var clave = localStorage.key(i);
+                var estudiante = $.parseJSON(localStorage.getItem(clave));
+                var estudianteAsistencia = parseInt(estudiante.asistencia);
+                j=i+1;
+                uno = estudianteAsistencia;
+                dos = parseInt(uno+dos);
+                total = Math.round(dos/j);
+            }
+            if(total<5){
+                alert("La clase tiene baja asistencia, el promedio es : "+total+" en Asistencia General");
+            }else if(total>5 && total<8){
+                alert("La clase es regularmente Asistida, el promedio es : "+total+" en Asistencia General");
+            }else if(total ===10){
+                alert("La clase es altamente asistida, el promedio es : "+total+" en Asistencia General");
+            }
+        });
+        ///Funcion para mostrar la Cantidad de Mujeres y Varones en la Lista de Estudiantes
+        $("#mostrar-genero").click(function () {
+            var f = 0;
+            var m = 0;
+            var total =0;
+            for (var i = 0; i < localStorage.length; i++) {
+                var clave = localStorage.key(i);
+                var estudiante = $.parseJSON(localStorage.getItem(clave));
+                if (estudiante.genero==="F" || estudiante.genero==="f") {
+                    f = f+1;
+                }
+                else{
+                    m = m+1;
+                }
+                total=f+m;
+            }
+            alert("La cantidad de mujeres es : "+f+" y la cantidad de hombres es : "+m+" el total de alumnos en el aula es : "+total);
+        });
     }
 
-
+    ///Funcion para Eliminar Estudiante de la Lista de Estudiantes
     function eliminarEstudiante(id) {
         localStorage.removeItem(id);
         listarEstudiante();
@@ -249,14 +138,14 @@
         }
 
         $("#codigo").val(contador);
-
+        ///Procedimiento para Guardar un Nuevo Estudiante
         $("#boton1").click(function () {
             var codigo=$("#codigo").val();
             var nombre=$("#nombre").val();
             var nota=$("#nota").val();
             var asistencia=$("#asistencia").val();
             var genero=$("#genero").val();
-
+            ///Creando el objeto Estudiante
             var estudiante={
                 codigo:codigo,
                 nombre:nombre,
@@ -264,22 +153,19 @@
                 asistencia:asistencia,
                 genero:genero
             };
+
             localStorage.setItem(codigo, JSON.stringify(estudiante));
             contador = localStorage.length+1;
-
-            // localStorage.setItem("jsonData", jsonData);
-            // localStorage.setItem("jsonData", JSON.stringify(jsonData));
-            // var jsonDataa = JSON.parse(localStorage.getItem("jsonData"));
-            // console.log(jsonDataa);
 
             listarEstudiante();
             reestablecer();
         });
 
+        ///Procedimiento para Reestablecer el Formulario Estudiante
         $("#boton2").click(function () {
             reestablecer(9);
         });
-
+        ///Funcion que funciona para reestablecer el Formulario Estudiante
         function reestablecer() {
             $("#codigo").val(contador);
             $("#nombre").val("");
@@ -294,47 +180,5 @@
             localStorage.clear();
         });
 
-        //Funcion que permite calcular la cantiudad de Hombres y Mujeres en la Tabla Estudiantes
-        function obtenerGenero(){
-            // for(var i=0; i<localStorage.length;i++)
-            // {
-            //     var clave = localStorage.key(i);
-            //     var estudiante = $.parseJSON(localStorage.getItem(clave));
-            //     var estudianteGenero = {};
-            //     // var clave = localStorage.key(i);
-            //     localStorage.setItem("estudianteGenero", estudianteGenero);
-            //     localStorage.setItem("estudianteGenero",JSON.stringify(estudiante));
-            //     // var estudianteGenero = $.parseJSON(localStorage.getItem(clave));
-            //     var estGen = JSON.parse(localStorage.getItem("estudianteGenero"));
-            //    console.log(estGen);
-            //     // localStorage.setItem("jsonData", estudiante);
-            //     // localStorage.setItem("jsonData", JSON.stringify(estudiante));
-            //     // var jsonDataa = JSON.parse(localStorage.getItem("jsonData"));
-            //     // console.log(jsonDataa);
-            //     console.log(estudianteGenero.genero);
-            //
-            //    localStorage.nota = document.getElementById("nota").value;
-            //     // if((localStorage[0])===3){
-            //     //     alert("nota es 3");
-            //     // }
-            //
-            //     console.log(localStorage.nota)
-
-            // }
-
-
-        }
-        //
-        // $("#mostrar-genero").click(function () {
-        //
-        //     var nota=$("#nota").val();
-        //     var nota={
-        //         nota:nota
-        //     };
-        //     localStorage.setItem("notata", JSON.stringify(nota));
-        //     var notata= JSON.parse(localStorage.getItem("notata"));
-        //     console.log(notata);
-        // });
-
     });
-// });
+
